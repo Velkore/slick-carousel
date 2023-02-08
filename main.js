2288407cf88d0a82c4dcb9415548dc1feb4fc7c9
Josh <jasmith0401@gmail.com>
2023-02-08 01:35:57 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.single-slide').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        variableWidth: true,
+        variableHeight: true
+    });
+})
